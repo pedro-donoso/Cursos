9f72eb714f12bc5f9a4915bb7223ecffd0ce1d1a
odikil <pedro.donoso.r@usach.cl>
2021-09-26 23:07:07 -0300

    cambio nombres

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,7 +3,7 @@ import {
 } from "./investigados.js"
 
 const data = (async function() {
-    const animalElement = document.querySelector("#animal")
+    const cursoElement = document.querySelector("#curso")
     const edadElement = document.querySelector("#edad")
     const comentariosElement = document.querySelector("#comentarios")
     const previewElement = document.querySelector("#preview")
@@ -16,10 +16,10 @@ const data = (async function() {
     let Animales = [];
 
     try {
-        const Request = await fetch("/animales.json");
+        const Request = await fetch("/cursos.json");
         const ParsedRequest = await Request.json();
 
-        Animales = ParsedRequest.animales;
+        Animales = ParsedRequest.cursos;
 
     } catch (e) {
         console.error(e)
@@ -31,31 +31,30 @@ const data = (async function() {
 
         elementos.innerHTML = ""
 
-        tarjetas.forEach(animal => {
+        tarjetas.forEach(tarjeta__curso => {
             const carta = document.createElement("div")
             const fotito = document.createElement("div");
             const botonera = document.createElement("div");
 
             carta.classList.add("card", "text-white", "bg-primary")
             carta.style.width = "50%";
-            fotito.innerHTML = `<img src="./assets/imgs/${animal.Img}" class="card-img-top"/>`;
+            fotito.innerHTML = `<img src="./assets/imgs/${tarjeta__curso.Img}" class="card-img-top"/>`;
 
             fotito.addEventListener("click", () => {
                 $("#modal").modal("show");
                 const modalBody = document.querySelector(".modal-body");
                 modalBody.innerHTML = `
-                <img src="./assets/imgs/${animal.Img}" style="width: 50%" class="img-fluid" />
-                <p class="text-white text-center" style="font-size: 14px"> ${animal.Nombre}</p>
-                <p class="text-white text-center" style="font-size: 14px">${animal.Edad}</p>
-                <p class="text-white text-center" style="font-size: 14px">${animal.Comentarios}</p>
+                <img src="./assets/imgs/${tarjeta__curso.Img}" style="width: 50%" class="img-fluid" />
+                <p class="text-white text-center" style="font-size: 14px"> ${tarjeta__curso.Nombre}</p>
+                <p class="text-white text-center" style="font-size: 14px">${tarjeta__curso.Edad}</p>
+                <p class="text-white text-center" style="font-size: 14px">${tarjeta__curso.Comentarios}</p>
                 `
             })
 
 
             botonera.addEventListener("click", () => {
-                if (animal.Nombre === "Full") {
-                    animal.Rugir(playerElement);
-                } else if (animal.Nombre === "React") {
+                if (tarjeta__curso.Nombre === "Full") {
+                } else if (tarjeta__curso.Nombre === "React") {
                 }
             })
 
@@ -69,20 +68,20 @@ const data = (async function() {
 
     }
 
-    animalElement.addEventListener("change", () => {
-        const eleccion = animalElement.value;
-        const hallado = Animales.find(animal => animal.name === eleccion)
+    cursoElement.addEventListener("change", () => {
+        const eleccion = cursoElement.value;
+        const hallado = Animales.find(curso__dato => curso__dato.name === eleccion)
 
         previewElement.setAttribute("src", `./assets/imgs/${hallado.imagen}`)
     });
 
     RegistrarElement.addEventListener("click", () => {
-        const nombre = animalElement.value;
+        const nombre = cursoElement.value;
         const edad = edadElement.value;
         const comentarios = comentariosElement.value;
         const {
             imagen
-        } = Animales.find(animal => animal.name === nombre)
+        } = Animales.find(curso__dato => curso__dato.name === nombre)
 
         switch (nombre) {
              case "Full": {
