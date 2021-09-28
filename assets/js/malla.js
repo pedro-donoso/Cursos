a134b22a2ec345c13f2fba83692a6d07456562a8
odikil <pedro.donoso.r@usach.cl>
2021-09-28 14:54:46 -0300

    comento malla.js

--- assets/js/malla.js
+++ assets/js/malla.js
@@ -1,29 +1,22 @@
 // importacion Curso.js
 import { Curso } from "./Curso.js"
-// importacion Curso.js
 
-// creo clases
+// creo subclase para cada curso con argumentos
 class VueJs extends Curso {
     constructor(...args) {
         super(...args)
     }
-
 };
 class React extends Curso {
     constructor(...args) {
         super(...args)
     }
-
 };
 class Angular extends Curso {
     constructor(...args) {
         super(...args)
     }
-
 };
-
-// creo clases
 
 // exportacion
 export { Curso, VueJs, React, Angular };
-// exportacion
