1fe696b8b1e73cd45dccdb2bb5069e859ba943f8
odikil <pedro.donoso.r@usach.cl>
2021-09-27 22:38:54 -0300

    metodologia bem

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,121 +6,133 @@ import {
 } from "./malla.js"
 // importo desde malla.js
 
+
+
 // creo constantes
-const cursoElement = document.querySelector("#curso")
-const sedeElement = document.querySelector("#sede")
-const modalidadElement = document.querySelector("#modalidad")
-const comentariosElement = document.querySelector("#comentarios")
-const previewElement = document.querySelector("#preview")
-const RegistrarElement = document.querySelector("#Registrar")
+const Course = document.querySelector("#curso")
+const Course__sede = document.querySelector("#sede")
+const Course__modalidad = document.querySelector("#modalidad")
+const Course__comentarios = document.querySelector("#comentarios")
+const Course__preview = document.querySelector("#preview")
+const Course__registrar = document.querySelector("#Registrar")
 // creo constantes
 
-let btnPop = document.getElementById('btnPop')
+// borrar, viene desde html
+let Course__borrar = document.getElementById('Borrar')
 
-//    creo array vacío cards
-let cards = [];
-let Cursos = [];
+// creo array vacío Course__cards
+let Course__cards = [];
+let Course__array = [];
 
-//    creo array vacío cards
+//    requiero json
 try {
     const Request = await fetch("/cursos.json");
     const ParsedRequest = await Request.json();
 
-    Cursos = ParsedRequest.cursos;
+    // capturo opciones, viene desde cursos.json
+    Course__array = ParsedRequest.opciones;
 
 } catch (e) {
     console.error(e)
 }
+//    requiero json
 
-// creo función vista
-function vista() {
-    // selecciono array vacío cards
-    const elementos = document.querySelector(".cards");
-    elementos.innerHTML = ""
-    // recorro cards  creo esctructura
-    cards.forEach(tarjeta__curso => {
-        const tarjeta = document.createElement("div")
-        const imagen = document.createElement("div");
-        const botonera = document.createElement("div");
-        tarjeta.classList.add("card", "text-white", "bg-primary")
-        tarjeta.style.width = "50%";
+// creo función Course__vista
+function Course__vista() {
+    // sCourse__elecciono cards, viene desde html
+    const Course__elementos = document.querySelector(".cards");
+    Course__elementos.innerHTML = ""
+    // recorro Course__cards  creo esctructura
+    Course__cards.forEach(Course__tarjeta => {
+        const Course__contenido = document.createElement("div")
+        const Course__imagen = document.createElement("div");
+        const Course__boton = document.createElement("div");
+        Course__contenido.classList.add("card", "text-white", "bg-primary")
+        Course__contenido.style.width = "50%";
         // envio tarjeta al panel
-        imagen.innerHTML = `<img src="./assets/imgs/${tarjeta__curso.Img}" class="card-img-top"/>`;
-        // evento click modal
-        imagen.addEventListener("click", () => {
+        Course__imagen.innerHTML = `<img src="./assets/imgs/${Course__tarjeta.Img}" class="card-img-top"/>`;
+
+        // evento click modal, viene desde html
+        Course__imagen.addEventListener("click", () => {
             $("#modal").modal("show");
 
-            console.log(tarjeta__curso);
+            console.log(Course__tarjeta);
 
+            // sCourse__elecciono modal-body, viene desde html
             const modalBody = document.querySelector(".modal-body");
             console.log(modalBody);
+
+            // inyecto estructura
             modalBody.innerHTML = `
-                <img src="./assets/imgs/${tarjeta__curso.Img}" style="width: 50%" class="img-fluid" />
-                <p class="text-white text-center" style="font-size: 16px">Curso: ${tarjeta__curso.Curso}</p>
-                <p class="text-white text-center" style="font-size: 16px">Sede: ${tarjeta__curso.Sede}</p>
-                 <p class="text-white text-center" style="font-size: 16px">Modalidad: ${tarjeta__curso.Modalidad}</p>
-                <p class="text-white text-center" style="font-size: 16px">Comentarios: ${tarjeta__curso.Comentarios}</p>
+                <img src="./assets/imgs/${Course__tarjeta.Img}" style="width: 50%" class="img-fluid" />
+                <p class="text-white text-center" style="font-size: 16px">Curso: ${Course__tarjeta.Curso}</p>
+                <p class="text-white text-center" style="font-size: 16px">Sede: ${Course__tarjeta.Sede}</p>
+                 <p class="text-white text-center" style="font-size: 16px">Modalidad: ${Course__tarjeta.Modalidad}</p>
+                <p class="text-white text-center" style="font-size: 16px">Comentarios: ${Course__tarjeta.Comentarios}</p>
                 `
         })
 
-        botonera.addEventListener("click", () => {
-            if (tarjeta__curso.Nombre === "VueJs") {
-            } else if (tarjeta__curso.Nombre === "React") {
-            } else if (tarjeta__curso.Nombre === "Angular") {
+        Course__boton.addEventListener("click", () => {
+            if (Course__tarjeta.Nombre === "VueJs") {
+            } else if (Course__tarjeta.Nombre === "React") {
+            } else if (Course__tarjeta.Nombre === "Angular") {
             }
         })
-        tarjeta.appendChild(imagen)
-        tarjeta.appendChild(botonera)
-        elementos.appendChild(tarjeta)
+        Course__contenido.appendChild(Course__imagen)
+        Course__contenido.appendChild(Course__boton)
+        Course__elementos.appendChild(Course__contenido)
         // evento click modal
     })
 
 }
 
-// creo función vista
-cursoElement.addEventListener("change", () => {
-    const eleccion = cursoElement.value;
-    const buscar = Cursos.find(curso__dato => curso__dato.name === eleccion)
+// Course principal
+Course.addEventListener("change", () => {
+    const Course__eleccion = Course.value;
 
-    previewElement.setAttribute("src", `./assets/imgs/${buscar.imagen}`)
+    // capturo names desde cursos.json
+    const Course__buscar = Course__array.find(Course__dato => Course__dato.names === Course__eleccion)
+
+    // capturo imagenes dessde cursos.json
+    Course__preview.setAttribute("src", `./assets/imgs/${Course__buscar.imagenes}`)
 });
 
-RegistrarElement.addEventListener("click", () => {
-    const curso = cursoElement.value;
-    const sede = sedeElement.value;
-    const modalidad = modalidadElement.value;
-    const comentarios = comentariosElement.value;
-    const {
-        imagen
-    } = Cursos.find(curso__dato => curso__dato.name === curso)
+// contenido cards
+Course__registrar.addEventListener("click", () => {
+    const curso = Course.value;
+    const sede = Course__sede.value;
+    const modalidad = Course__modalidad.value;
+    const comentarios = Course__comentarios.value;
+    const {imagenes} = Course__array.find(Course__dato => Course__dato.names === curso)
 
     switch (curso) {
         case "VueJs": {
-            const vuejs = new VueJs(curso, sede, modalidad, imagen, comentarios);
-            cards.push(vuejs);
+            const vuejs = new VueJs(curso, sede, modalidad, imagenes, comentarios);
+            Course__cards.push(vuejs);
         }
             break;
         case "React": {
-            const react = new React(curso, sede, modalidad, imagen, comentarios);
-            cards.push(react);
+            const react = new React(curso, sede, modalidad, imagenes, comentarios);
+            Course__cards.push(react);
         }
             break;
         case "Angular": {
-            const angular = new Angular(curso, sede, modalidad, imagen, comentarios);
-            cards.push(angular);
+            const angular = new Angular(curso, sede, modalidad, imagenes, comentarios);
+            Course__cards.push(angular);
         }
             break;
 
     }
 
     console.log({
-        cards
+        Course__cards
     })
 
-    btnPop.addEventListener('click', function () {
-        cards = [];
-        vista();
-    })
-
-    vista();
+    Course__borrar.addEventListener('click', function () {
+    Course__cards = [];
+    Course__vista();
 })
+
+    Course__vista();
+})
+// contenido cards
