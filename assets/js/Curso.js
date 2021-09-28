c68ae7af8ee74bae795b907dbdcb83cfc3b01a62
odikil <pedro.donoso.r@usach.cl>
2021-09-28 14:27:41 -0300

    comento curso.js

--- assets/js/Curso.js
+++ assets/js/Curso.js
@@ -1,48 +1,36 @@
-// clase
+// creo clase con campos llenados y se inicializa con constructor
 export class Curso {
     #curso;
     #sede;
     #modalidad;
     #img;
     #participantes;
-   
-
-    // constructor
+// creo constructor
     constructor(curso, sede, modalidad, img, participantes){
         this.#curso=curso;
         this.#sede = sede;
         this.#modalidad = modalidad;
         this.#img=img;
         this.#participantes = participantes;
-  
     }
-  // constructor
 
-    // gets
+    // gets, conexion con id de index.js, set pasa el value
     get Curso(){
         return this.#curso;
     }
     get Sede(){
         return this.#sede;
     }
-
     get Modalidad() {
         return this.#modalidad;
     }
-    
     get Img2(){
         return this.#img;
     }
-
     get Participantes(){
         return this.#participantes;
     }
-
-
-
     set Participantes(value){
         this.#participantes=value;
     }
-     // gets
 }
-// clase
